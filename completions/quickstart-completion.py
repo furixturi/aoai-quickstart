dc34f17f2fb2a72c3c3538772f9b7ec331a41d11
Xiaoli Shen <xiaolishen@microsoft.com>
2024-03-29 11:39:25 +0900

    completion quickstart code

---
+++ completions/quickstart-completion.py
@@ -0,0 +1,24 @@
+# Official Doc Quickstarts > Completion > Python
+# https://learn.microsoft.com/en-us/azure/ai-services/openai/quickstart?tabs=bash%2Cpython-new&pivots=programming-language-python
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+import os
+from openai import AzureOpenAI
+
+client = AzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+)
+
+deployment_name = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME")
+
+print("Sending a test completion job")
+start_phrase = "Write a tagline for an ice cream shop."
+response =client.completions.create(model=deployment_name, 
+                                    prompt=start_phrase, 
+                                    max_tokens=10)
+print(start_phrase+response.choices[0].text)
